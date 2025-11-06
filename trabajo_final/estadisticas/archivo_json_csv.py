abbe2ca7f40f9c86c80152a3df4f9516cc131646
manucourtade <courtademanuel@outlook.es>
2025-11-05 21:51:58 -0300

    ADD: Mandamos los puntajes registrados a un csv y vamos a hacer el archivo json

--- trabajo_final/estadisticas/archivo_json_csv.py
+++ trabajo_final/estadisticas/archivo_json_csv.py
@@ -1,15 +1,15 @@
 import os
 
 archivo = 'estadisticas/historial.csv'
-
+archivo_json = 'estadisticas/niveles.json'
 
 def realizar_registro(nombre_archivo, nombre, puntos):
-        
     if not os.path.exists(nombre_archivo):
         with open(nombre_archivo, 'w') as archivo:
             archivo.write("=" * 30 + '\n    NOMBRE   |    PUNTAJE \n' + '=' * 30 +'\n')
     with open(nombre_archivo, 'a') as archivo:
         archivo.write(f'   {nombre.strip():<10}|    {puntos.strip():>7}\n')
+
 
 def leer_archivo_csv(nombre_archivo):
     if not os.path.exists(nombre_archivo):
@@ -18,6 +18,11 @@ def leer_archivo_csv(nombre_archivo):
     
     with open(nombre_archivo, 'r') as archivo:
         texto = archivo.read()
-        print(texto)
+    return texto
+
+
+def json_tematicas(nombre_archivo):
+    pass
+
 
 
