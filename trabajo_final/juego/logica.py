4b708c51f183ce3018bb1497993e43252304ecd8
manucourtade <courtademanuel@outlook.es>
2025-11-05 21:32:17 -0300

    ADD: Mandamos los puntajes registrados a un csv

--- trabajo_final/juego/logica.py
+++ trabajo_final/juego/logica.py
@@ -1,24 +1,35 @@
 import random
-from puntaje.plantilla_puntaje import tabla_puntajes, posibles_jugadas
+from puntaje.plantilla_puntaje import tabla_puntajes, posibles_jugadas, jugada_generala
+from estadisticas.archivo_json_csv import realizar_registro, archivo
 
 def tirar_dado(dados):
     while len(dados) < 5:
         dado = random.randint(1, 6)
         dados.append(dado)
 
+
 def ronda():
-    # ✅ Se inicializa solo una vez al comienzo de la partida
+
+    simbolos = {
+        1: 'Pikachu',
+        2: 'Bulbasur',
+        3: 'Charmander',
+        4: 'Squirtle',
+        5: 'Snorlax',
+        6: 'Gengar'
+    }
+
     puntajes = {
-        1: None, 
-        2: None,  
-        3: None,  
-        4: None, 
-        5: None,
-        6: None,  
-        7: None,  
-        8: None, 
-        9: None,  
-        10: None  
+        1:'--', 
+        2: '--',  
+        3: '--',  
+        4: '--', 
+        5: '--',
+        6: '--',  
+        7: '--',  
+        8: '--', 
+        9: '--',  
+        10: '--'  
     }
 
     cant_categorias = 10
@@ -27,25 +38,36 @@ def ronda():
 
     while cant_categorias > 0:
         print(f'\n--- INICIO DE RONDA. Categorías restantes: {cant_categorias} ---\n')
-        dados = []  # reinicia los dados para cada ronda
-
+        dados = []  
         for turno in range(turnos):
             print(f'\n<<< TURNO JUGADOR - TIRO {turno + 1} de {turnos} >>>')
-            print('-' * 40)
-            print('Posición: (1\t) | (2\t) | (3\t) | (4\t) | (5\t) |')
-            print('         ',('-' * 39), '|')
+            print('         ',('-' * 61), '|')
+            print('Posición: (1\t)   | (2\t)| (3\t)     | (4\t)  | (5\t )\t|')
+            print('         ',('-' * 61), '|')
 
             tirar_dado(dados)
-            print(f'Valor: \t {dados[0]:^5}\t  | {dados[1]:^2} \t  | {dados[2]:^2} \t  | {dados[3]:^2} \t  | {dados[4]:^2} \t  |')
+            print(f'Simbolo: {simbolos[dados[0]]:^10} | {simbolos[dados[1]]:^10} | {simbolos[dados[2]]:^10} | {simbolos[dados[3]]:^10} | {simbolos[dados[4]]:^10} |')  
+            print('         ',('-' * 61), '|')
+            print(f'Valor: \t {dados[0]:^5}\t    | {dados[1]:^2} \t | {dados[2]:^2} \t      |    {dados[3]:^2} \t   | {dados[4]:^2} \t|')
 
-            # 🔹 Solo permitir elegir dados en los dos primeros tiros
-            if turno < 3:
+            puntos_generala = jugada_generala(dados, turno + 1)
+
+            if puntos_generala == 100:  
+                print('\n🎉 ¡GENERALA SERVIDA! Fin del juego 🎉')
+                puntos = 100
+                cant_categorias = 0
+                break
+            elif puntos_generala == 50:  
+                print('\n¡Generala! +50 puntos')
+                puntos = 50
+
+            elif turno < 3:
                 desea_conservar = input(
                     'Ingrese las posiciones de los dados a conservar (1-5), separadas por coma, o ENTER para tirar todos: '
                 ).strip()
 
                 if desea_conservar == "":
-                    dados = []  # tirar todos
+                    dados = []  
                 else:
                     posiciones = desea_conservar.split(",")
                     dados_conservados = []
@@ -67,10 +89,22 @@ def ronda():
                     if len(dados) == 5:
                         print('Has conservado los 5 dados')
                         break
-
         categoria, puntos = posibles_jugadas(dados, puntajes)
         puntajes[categoria] = puntos  
-        tabla_puntajes(puntajes)
+        total_puntos = tabla_puntajes(puntajes)
         cant_categorias -= 1
+
+        
+    nombre = ''
+    while nombre == '':
+        nombre = input('Ingrese su nombre para registrar: ').strip().title()
+        if nombre ==  '':
+            print('No puede ingresar nombres en blanco!')
+
+    total_puntos = str(total_puntos)
+    realizar_registro(archivo, nombre, total_puntos)
+    print(f"\nRegistro guardado correctamente: {nombre} -> {total_puntos} puntos.\n")
+        
+
 
      
