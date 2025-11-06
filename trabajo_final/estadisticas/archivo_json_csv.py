b44baa721f6e66ac9a3a88d01450c562cf42fb79
manucourtade <courtademanuel@outlook.es>
2025-11-05 21:33:42 -0300

    ADD: Estamos haciendo el historial de partidas!

---
+++ trabajo_final/estadisticas/archivo_json_csv.py
@@ -0,0 +1,23 @@
+import os
+
+archivo = 'estadisticas/historial.csv'
+
+
+def realizar_registro(nombre_archivo, nombre, puntos):
+        
+    if not os.path.exists(nombre_archivo):
+        with open(nombre_archivo, 'w') as archivo:
+            archivo.write("=" * 30 + '\n    NOMBRE   |    PUNTAJE \n' + '=' * 30 +'\n')
+    with open(nombre_archivo, 'a') as archivo:
+        archivo.write(f'   {nombre.strip():<10}|    {puntos.strip():>7}\n')
+
+def leer_archivo_csv(nombre_archivo):
+    if not os.path.exists(nombre_archivo):
+        print('NO HAS JUGADO NINGUNA PARTIDA TODAVIA!')
+        return
+    
+    with open(nombre_archivo, 'r') as archivo:
+        texto = archivo.read()
+        print(texto)
+
+
